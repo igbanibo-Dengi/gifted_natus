1347d33a4b07e30231abca972c4d16996b743c81
dengi <mcmorgan6560@gmail.com>
2023-10-06 22:54:18 +0100

    another fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
-    <main className="container pt-36">
+    <main className="container pt-20 md:pt-36">
       <h1 className="leading-none text-[28px] md:text-[35px] font-bold capitalize text-center mb-10">
         Our Stories
       </h1>
