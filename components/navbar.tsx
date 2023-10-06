82f45a110f293079997e280eaa0487848d7065fd
dengi <mcmorgan6560@gmail.com>
2023-10-06 22:28:07 +0100

    fixed nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import React, { useState } from "react";
-import Image from 'next/image';
-import { ModeToggle } from './modeToggle';
-
-
+import Image from "next/image";
+import { ModeToggle } from "./modeToggle";
 
 const NavBar = () => {
   const [navbar, setNavbar] = useState(false);
@@ -17,22 +15,22 @@ const NavBar = () => {
       <div className="justify-between md:items-center md:flex container mx-auto px-5">
         <div>
           <div className="flex items-center justify-between">
-            <Link href="/" className='pt-2 flex items-end'>
+            <div className="pt-2 flex items-end">
               <Image
                 src="/logo-trans.png"
-                alt='logo-trans'
+                alt="logo-trans"
                 width={50}
                 height={50}
-                className=' md:hidden'
+                className=" md:hidden"
               />
               <Image
                 src="/logo-trans.png"
-                alt='logo-trans'
+                alt="logo-trans"
                 width={80}
                 height={80}
-                className='hidden md:block'
+                className="hidden md:block"
               />
-            </Link>
+            </div>
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
@@ -51,16 +49,19 @@ const NavBar = () => {
                       clipRule="evenodd"
                     />
                   </svg>
-
-
-
                 ) : (
-
-
-                  <svg width="19" height="14" viewBox="0 0 19 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.35714 0H8.14286C8.50279 0 8.84799 0.1475 9.1025 0.410051C9.35701 0.672601 9.5 1.0287 9.5 1.4C9.5 1.7713 9.35701 2.1274 9.1025 2.38995C8.84799 2.6525 8.50279 2.8 8.14286 2.8H1.35714C0.997206 2.8 0.652012 2.6525 0.397498 2.38995C0.142984 2.1274 0 1.7713 0 1.4C0 1.0287 0.142984 0.672601 0.397498 0.410051C0.652012 0.1475 0.997206 0 1.35714 0ZM10.8571 11.2H17.6429C18.0028 11.2 18.348 11.3475 18.6025 11.6101C18.857 11.8726 19 12.2287 19 12.6C19 12.9713 18.857 13.3274 18.6025 13.5899C18.348 13.8525 18.0028 14 17.6429 14H10.8571C10.4972 14 10.152 13.8525 9.8975 13.5899C9.64298 13.3274 9.5 12.9713 9.5 12.6C9.5 12.2287 9.64298 11.8726 9.8975 11.6101C10.152 11.3475 10.4972 11.2 10.8571 11.2ZM1.35714 5.6H17.6429C18.0028 5.6 18.348 5.7475 18.6025 6.01005C18.857 6.2726 19 6.6287 19 7C19 7.3713 18.857 7.7274 18.6025 7.98995C18.348 8.2525 18.0028 8.4 17.6429 8.4H1.35714C0.997206 8.4 0.652012 8.2525 0.397498 7.98995C0.142984 7.7274 0 7.3713 0 7C0 6.6287 0.142984 6.2726 0.397498 6.01005C0.652012 5.7475 0.997206 5.6 1.35714 5.6Z" fill="currentColor" />
+                  <svg
+                    width="19"
+                    height="14"
+                    viewBox="0 0 19 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1.35714 0H8.14286C8.50279 0 8.84799 0.1475 9.1025 0.410051C9.35701 0.672601 9.5 1.0287 9.5 1.4C9.5 1.7713 9.35701 2.1274 9.1025 2.38995C8.84799 2.6525 8.50279 2.8 8.14286 2.8H1.35714C0.997206 2.8 0.652012 2.6525 0.397498 2.38995C0.142984 2.1274 0 1.7713 0 1.4C0 1.0287 0.142984 0.672601 0.397498 0.410051C0.652012 0.1475 0.997206 0 1.35714 0ZM10.8571 11.2H17.6429C18.0028 11.2 18.348 11.3475 18.6025 11.6101C18.857 11.8726 19 12.2287 19 12.6C19 12.9713 18.857 13.3274 18.6025 13.5899C18.348 13.8525 18.0028 14 17.6429 14H10.8571C10.4972 14 10.152 13.8525 9.8975 13.5899C9.64298 13.3274 9.5 12.9713 9.5 12.6C9.5 12.2287 9.64298 11.8726 9.8975 11.6101C10.152 11.3475 10.4972 11.2 10.8571 11.2ZM1.35714 5.6H17.6429C18.0028 5.6 18.348 5.7475 18.6025 6.01005C18.857 6.2726 19 6.6287 19 7C19 7.3713 18.857 7.7274 18.6025 7.98995C18.348 8.2525 18.0028 8.4 17.6429 8.4H1.35714C0.997206 8.4 0.652012 8.2525 0.397498 7.98995C0.142984 7.7274 0 7.3713 0 7C0 6.6287 0.142984 6.2726 0.397498 6.01005C0.652012 5.7475 0.997206 5.6 1.35714 5.6Z"
+                      fill="currentColor"
+                    />
                   </svg>
-
                 )}
               </button>
             </div>
@@ -68,16 +69,56 @@ const NavBar = () => {
         </div>
         <div>
           <div
-            className={`flex-1 justify-self-center pb-3 h-screen sm:h-fit mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
-              }`}
+            className={`flex-1 justify-self-center pb-3 h-screen sm:h-fit mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
           >
             <div className="flex flex-col h-screen md:h-fit md:flex-row md:justify-center md:items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-              <Link onClick={() => setNavbar(!navbar)} href='/' className={pathname === '/' ? 'text-primary underline' : 'hover:text-primary'}>Home</Link>
-              <Link onClick={() => setNavbar(!navbar)} href='/about' className={pathname === '/about' ? 'text-primary underline' : 'hover:text-primary'}>About Us</Link>
-              <Link onClick={() => setNavbar(!navbar)} href='/blog' className={pathname === '/contact' ? 'text-primary underline' : 'hover:text-primary'}>Blog</Link>
-              <Link onClick={() => setNavbar(!navbar)} href='/contact' className={pathname === '/contact' ? 'text-primary underline' : 'hover:text-primary'}>Contact</Link>
+              <Link
+                onClick={() => setNavbar(!navbar)}
+                href="/"
+                className={
+                  pathname === "/"
+                    ? "text-primary underline"
+                    : "hover:text-primary"
+                }
+              >
+                Home
+              </Link>
+              <Link
+                onClick={() => setNavbar(!navbar)}
+                href="/about"
+                className={
+                  pathname === "/about"
+                    ? "text-primary underline"
+                    : "hover:text-primary"
+                }
+              >
+                About Us
+              </Link>
+              <Link
+                onClick={() => setNavbar(!navbar)}
+                href="/blog"
+                className={
+                  pathname === "/contact"
+                    ? "text-primary underline"
+                    : "hover:text-primary"
+                }
+              >
+                Blog
+              </Link>
+              <Link
+                onClick={() => setNavbar(!navbar)}
+                href="/contact"
+                className={
+                  pathname === "/contact"
+                    ? "text-primary underline"
+                    : "hover:text-primary"
+                }
+              >
+                Contact
+              </Link>
               <ModeToggle />
-
             </div>
           </div>
         </div>
